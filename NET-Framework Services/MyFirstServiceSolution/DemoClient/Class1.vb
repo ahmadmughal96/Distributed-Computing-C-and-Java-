e8f1fb862b762e8d8fd5a0260fc5cda31fcbf243
Ahmad Amjad Mughal <63554130+ahmadmughal96@users.noreply.github.com>
2020-04-14 00:57:25 +0500

    Add files via upload

---
+++ NET-Framework Services/MyFirstServiceSolution/DemoClient/Class1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Class1
+{
+
+static void Main(string[] args)
+
+{
+
+localhost.MathServiceClient obj = new DemoClient.localhost.MathServiceClient();
+
+localhost.Math mathobj = new DemoClient.localhost.Math();
+
+mathobj.Number1 = 10;
+
+mathobj.Number2 = 5;
+
+Console.WriteLine("Addition continues..");
+
+Console.WriteLine(obj.Addition(mathobj));
+
+Console.WriteLine("Subtraction continues..");
+
+Console.WriteLine(obj.Subtraction(mathobj));
+
+Console.WriteLine("Multiplication continues..");
+
+Console.WriteLine(obj.Multiplication(mathobj));
+
+Console.WriteLine("Division continues..");
+
+Console.WriteLine(obj.Division(mathobj));
+
+Console.ReadLine();
+
+}
+
+}
+
+}
+
+End Class
---
+++ NET-Framework Services/MyFirstServiceSolution/MyServiceLibrary/MyService.cs
@@ -0,0 +1,61 @@
+﻿//Ahmad Amjad Mughal
+//121672
+//BSCS6C
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Linq;
+using System.Text;
+
+namespace MyServiceLibrary
+{
+    [DataContract]
+    public class Maths
+    {
+       
+        [DataMember]
+        public int Number1 { get; set; }
+        [DataMember]
+        public int Number2 { get; set; }
+    }
+    [ServiceContract]
+    public interface IMaths
+    {
+        [OperationContract]
+         int Addition(Maths obj1);
+        [OperationContract]
+         int Subtraction(Maths obj2);
+        [OperationContract]
+         int Multiplication(Maths obj3);
+        [OperationContract]
+         int Division(Maths obj4);
+		[OperationContract]
+		 int Power(Maths obj5); 
+    }
+    class MyService:IMaths
+    {
+        #region IMaths Members
+        public int Addition(Maths obj1)
+        {
+            return (obj1.Number1 + obj1.Number2);
+        }
+        public int Subtraction(Maths obj2)
+        {
+            return (obj2.Number1 - obj2.Number2);
+        }
+        public int Multiplication(Maths obj3)
+        {
+            return (obj3.Number1 * obj3.Number2);
+        }
+        public int Division(Maths obj4)
+        {
+            return (obj4.Number1 / obj4.Number2);
+        }
+		public int Power(Maths obj5)
+		{
+			return (Math.power(obj5.Number1,obj5,Number2));
+		}
+        #endregion
+    }
+}
